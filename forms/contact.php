76226dfb109e4e4155f79736f15d97803935e100
Sanjiv Tamang <sanjivtamang2020@gmail.com>
2023-05-22 14:08:23 -0500

    email
    receiving email update

--- forms/contact.php
+++ forms/contact.php
@@ -1,7 +1,7 @@
 <?php
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'sanjivtamang2020@gmail.com';
+  $receiving_email_address = 'sanjiv@triunetech.info';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
